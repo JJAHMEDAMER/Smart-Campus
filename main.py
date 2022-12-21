f1a68705056bcee2bdcec1c63f604e9b1be79c23
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-21 13:51:07 +0200

    capture images from video stream

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import cv2
+import os
+from time import sleep
+from uuid import uuid1
 
 # Video Stream
 cam = cv2.VideoCapture(0)
@@ -10,4 +13,25 @@ while True:
         break
     
 cam.release
+cv2.destroyAllWindows()
+
+
+# Capture images for training
+IMAGES_PATH = os.path.join('data','images')
+number_images = 10
+
+cam = cv2.VideoCapture(0)
+
+for imgnum in range(number_images):
+    print(f'Collecting image {imgnum}')
+    ret, frame = cam.read()
+    imgname = os.path.join(IMAGES_PATH,f'{str(uuid1())}.jpg')
+    cv2.imwrite(imgname, frame)
+    cv2.imshow('frame', frame)
+    sleep(0.5)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
 cv2.destroyAllWindows()
