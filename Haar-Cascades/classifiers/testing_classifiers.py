be585782776e407ac3889865cc156cb728e675f8
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-24 14:27:53 +0200

    calc avg_accuracy and total time

--- Haar-Cascades/classifiers/testing_classifiers.py
+++ Haar-Cascades/classifiers/testing_classifiers.py
@@ -1,6 +1,9 @@
 import json
 import cv2
 import os
+from time import time
+
+start = time()
 
 IMGS_DIR = 'test_images'
 img_path_list = os.listdir(IMGS_DIR)
@@ -9,6 +12,7 @@ face_detection = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fron
 
 
 img_data_list = []
+total_number_faces = 0
 for img_path in img_path_list:
     name, ext = os.path.splitext(img_path)
     number_of_faces = name.split('-')
@@ -19,7 +23,10 @@ for img_path in img_path_list:
         "number_of_detected_faces": 0,
         "Accuracy": 0
     })
+    
+    total_number_faces = total_number_faces + int(number_of_faces[0])
 
+total_number_detected = 0
 for img_data in img_data_list:
     img = cv2.imread(img_data["path"])
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -28,7 +35,18 @@ for img_data in img_data_list:
     # img , scale_factor, nearest neighbor
     
     img_data["number_of_detected_faces"] = len(faces)
+    total_number_detected = total_number_detected + len(faces)
     img_data["Accuracy"] = len(faces)/img_data["number_of_faces"] * 100
     
-with open('Haar-Cascades/classifiers/test_noResize.json', "w") as outfile:  
+
+end = time()
+
+img_data_list.append({"total_time": end - start})
+img_data_list.append({
+    "avg_accuracy": total_number_detected/total_number_faces*100,
+    'total_number_faces' : total_number_faces,
+    'total_number_detected': total_number_detected
+    })
+
+with open('Haar-Cascades/classifiers/results/default_noResize.json', "w") as outfile:  
     json.dump(img_data_list, outfile)
